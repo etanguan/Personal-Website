b7f0b5e3cd5ac81e39461343beb58aa07bb3d4c2
etanguan <guanethan123@gmail.com>
2023-03-14 00:03:00 -0400

    Added pictures and more colourful spacers

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -30,22 +30,28 @@ function Home() {
             
             <div className='home-body-section'>
                 <h1 className='home-body-intro'>This is what I do.</h1>
-                <div className="spacer wave-1">
+                <div className="spacer-250 wave-1">
 
                 </div>
-                <div className='home-body-content'>
-                    <div className='home-body-content-description'>
+                <div className='home-body-content purple'>
+                    <div className='home-body-content-description '>
+                        <img className="bg-blob"src="/images/greenBlob.png" alt="blob"/>
                         <h1 className='black-text'>I code.</h1>
+                        <p className='black-text'> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque aliquet non tellus vel faucibus. Sed dolor magna, iaculis sit amet luctus sit amet, maximus id elit. In erat ipsum, venenatis nec volutpat cursus, mattis id lorem. Ut ultricies velit elit, sed ultrices nibh aliquet sagittis.</p>
                     </div>
                     <div className="home-body-content-graphic grid">
-                        <div className='code-grid-item'>1</div>
+                        
+                        <div className='code-grid-item'>
+                            <img className="code-image" src="images/uniHealth.png" alt="uniHealth" />
+                        </div>
                         <div className='code-grid-item'>2</div>
                         <div className='code-grid-item'>3</div>
                         <div className='code-grid-item'>4</div>
                     </div>
 
                 </div>
-                <div className='home-body-content'>
+                <div className="spacer-200 wave-purple-green"></div>
+                <div className='home-body-content green'>
                     <div className="home-body-content-graphic">
                         <img src="/images/logo512.png" alt="temp" />
                     </div>
@@ -54,17 +60,30 @@ function Home() {
                     </div>
 
                 </div>
-                <div className='home-body-content'>
+                <div className="spacer-200 wave-green-purple"></div>
+                <div className='home-body-content purple'>
                     <div className='home-body-content-description'>
                         <h1 className='black-text'>I teach.</h1>
                     </div>
                     <div className="home-body-content-graphic rel">
-                        <span>1</span>
-                        <span>2</span>
-                        <span>3</span>
-                        <span>4</span>
-                        <span>5</span>
-                        <span>6</span>
+                        <span>
+                            <img className='teach-image' src="/images/problemSolving.png" alt="nothing" />
+                        </span>
+                        <span>
+                            <img className='teach-image' src="/images/git.png" alt="nothing" />
+                        </span>
+                        <span>
+                            <img className='teach-image' src="/images/binarySearch.png" alt="nothing" />
+                        </span>
+                        <span>
+                            <img className='teach-image' src="/images/timeComplexity.png" alt="nothing" />
+                        </span>
+                        <span>
+                            <img className='teach-image' src="/images/dynamicProgramming.png" alt="nothing" />
+                        </span>
+                        <span>
+                            <img className='teach-image' src="/images/dataStructures.png" alt="nothing" />
+                        </span>
 
 
                     </div>
